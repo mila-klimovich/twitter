7da1cff67e3271e468b7f43c25388811c02fef35
Mila Klimovich <mila.k.klimovich@gmail.com>
2024-06-18 17:24:34 +0300

    file deleted

--- public/assets/post_size.js
+++
@@ -1,7 +0,0 @@
-export default function postSize (string) {
-    const search = /(https?:\/\/[^\s]+)/g;
-    const replace = "";
-    const result = string.replaceAll(search, replace).length;
-
-    return result;
-}
