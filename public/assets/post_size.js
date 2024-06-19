055cf215466fd93208316842d7718ea4273250ed
Mila Klimovich <mila.k.klimovich@gmail.com>
2024-06-19 23:11:43 +0300

    postSize function modified

--- public/assets/post_size.js
+++ public/assets/post_size.js
@@ -1,7 +1,12 @@
 export default function postSize(string) {
-    const search = /(https?:\/\/[^\s]+)/g;
-    const replace = "";
-    const result = string.replaceAll(search, replace).length;
-
+    if (string.indexOf("<") > -1) {
+        const end = string.indexOf(">") + 1;
+        const beginning = string.indexOf("<");
+        string = string.split("");
+        string.splice(beginning, end - beginning);
+        string = string.join("");
+        return postSize(string);
+    }
+    const result = string.length;
     return result;
 }
