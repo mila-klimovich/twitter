4912281fc291583871b7fbdc13b48ef4fa61451a
Mila Klimovich <mila.k.klimovich@gmail.com>
2024-06-24 13:29:27 +0300

    post size merge conflict resolved

--- public/assets/post_size.js
+++ public/assets/post_size.js
@@ -1,12 +1,13 @@
-export default function postSize(string) {
-    if (string.indexOf("<") > -1) {
-        const end = string.indexOf(">") + 1;
-        const beginning = string.indexOf("<");
-        string = string.split("");
-        string.splice(beginning, end - beginning);
-        string = string.join("");
-        return postSize(string);
-    }
-    const result = string.length;
-    return result;
+export default function postSize(str) {
+  let string = str;
+  if (string.indexOf('<') > -1) {
+    const end = string.indexOf('>') + 1;
+    const beginning = string.indexOf('<');
+    string = string.split('');
+    string.splice(beginning, end - beginning);
+    string = string.join('');
+    return postSize(string);
+  }
+  const result = string.length;
+  return result;
 }
